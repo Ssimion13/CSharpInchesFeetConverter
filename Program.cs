caff006c40b54527b76a20cf7a20a4a29a8f4fc1
ssimion13 <scottsimion@gmail.com>
2018-10-16 00:04:11 -0700

    just a simple feet to inches converter. Nothing special but it isn't bad...

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convert to Inches or Feet?");
+            String typeOfItem = Console.ReadLine();
+            Console.WriteLine("How many?");
+            String amount = Console.ReadLine();
+            int convertedNumber = 0;
+            Int32.TryParse(amount, out convertedNumber);
+
+            bool inchesResult = typeOfItem.Equals("Inches", StringComparison.OrdinalIgnoreCase);
+            bool feetResult= typeOfItem.Equals("Feet", StringComparison.OrdinalIgnoreCase);
+
+
+            if(inchesResult == true)
+            {
+                Console.WriteLine("Turning " + convertedNumber + " Inches to Feet...");
+                Console.WriteLine(FeetToInches(convertedNumber));
+            }
+            else if (feetResult == true)
+            {
+                Console.WriteLine("Turning " + convertedNumber + " Feet to Inches");
+                Console.WriteLine(InchesToFeet(convertedNumber));
+            }
+            else
+            {
+                Console.WriteLine("incorrect input");
+            }
+            Console.WriteLine("Thanks for using this!");
+        }
+        static double FeetToInches(double feet)
+        {
+            return feet * 12;
+        }
+        static double InchesToFeet(double inches)
+        {
+            return inches / 12;
+        }
+    }
+}
